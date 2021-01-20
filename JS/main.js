2f0eb7ab2b9c2e8dfa4f4cf0d149ad6649663bee
Shin <shin@gmail.com>
2021-01-20 19:04:24 +0100

    implémentation des images

--- JS/main.js
+++ JS/main.js
@@ -7,8 +7,8 @@ $(function(){
     var $form = $("<section id='formulaire'></section>");
     $(".h2").after($form);
     $form.append("<form action='' method='get' class='form'><fieldset><legend>Insérer le titre et l'auteur : </legend></fieldset></form>");
-    var $title = $("fieldset").append("<label for='title'>Titre du livre * : </label><br/><input type='text' id='title' name='title' required/>");
-    var $author = $("fieldset").append("<br/><label for='author'>Auteur * : </label><br/><input type='text' id='author' name='author' required /><br>");
+    var $title = $("fieldset").append("<label for='title'>Titre du livre * : </label><br/><input type='text' id='titleInput' name='title' required/>"); 
+    var $author = $("fieldset").append("<br/><label for='author'>Auteur * : </label><br/><input type='text' id='authorInput' name='author' required/><br>"); 
     var $search = createButton("submit",'search',"Chercher").appendTo("fieldset");
     var $cancel = createButton("reset",'cancel',"Annuler");
     $cancel.appendTo("fieldset");
@@ -24,14 +24,13 @@ $(function(){
     $bookTitle = $('<h4>Titre : </h4>');
     $titleOutput = $('<span class="bookTitle"></span>');
     $bookId = $('<h5>Id : </h5>');
-    $idOutput = $('<span class="bookId">');
+    $idOutput = $('<span class="bookId"></span>');
     $bookAuthor = $('<h5>Auteur : </h5>');
     $authorOutput = $('<span class="bookAuthor"></span>')
     $bookDescription = $('<h5>Description : </h5>');
     $descriptionOutput = $('<span class="bookDescription"></span>');
-    $bookmarkLink = $('<a id="bookmark" href="#"></a>');
-    $bookmarkIcon = $('<img src="logo/bookmark-regular.svg"/>');
-    $bookPicture = $('<img class="unavailable" src="logo/unavailable.png"/>');
+    $bookmarkIcon = $('<img id="bookmark" src="logo/bookmark-regular.svg"/>');
+    $bookPicture = $('<img class="unavailable" src="logo/unavailable.png" alt="couverture du livre" title="Couverture"/>');
     $('#searchResults').append($bookModel);
     $($bookTitle).append($titleOutput);
     $('#bookModel').append($bookTitle);
@@ -42,8 +41,7 @@ $(function(){
     $($bookDescription).append($descriptionOutput)
     $('#bookModel').append($bookDescription);
     $('#bookModel').append($bookPicture);
-    $($bookTitle).append($bookmarkLink);
-    $($bookmarkLink).prepend($bookmarkIcon);
+    $($bookTitle).prepend($bookmarkIcon);
     
     // On ajoute l'évènement click sur le bouton "Ajouter un livre"
     $addBook.click(function(){
@@ -56,19 +54,20 @@ $(function(){
         $form.hide();
         $addBook.show();
     })
-    $("#title").keyup(function(){
-        var titre = $(this).val();
-        var auteur = $(this).val();
+    $(".search").click(function(event){
+        event.preventDefault();
+        var titre = $("#titleInput").val();
+        var auteur = $("#authorInput").val();
         console.log(titre + auteur);
         var recherche = 'intitle:' + titre + '+inauthor:' + auteur ;
-        if(titre.length>=3) {
-            //& auteur.length>=3){      // S'il y a un minimum de 3 caractères, on lance l'appel AJAX
+        if(titre.length>=3 && auteur.length>=3) {      // S'il y a un minimum de 3 caractères, on lance l'appel AJAX
                 $.ajax({
                     url : "https://www.googleapis.com/books/v1/volumes?q=" + recherche ,
                     method : "GET",
                     data : titre + auteur,
                     success : function(data){
                         console.log("ça fonctionne",data);
+                        console.log(titre);
                         var htmlContent = "";
                         //var itemTemplate = $bookModel;
                         $(data.items).each(function(i){      // for(var i=0;i<data.items.length;i++){
@@ -78,33 +77,50 @@ $(function(){
                             console.log("Auteur : " + data.items[i].volumeInfo.authors);
                             console.log(data.items[i].volumeInfo.description);
                             $titleOutput.text(data.items[i].volumeInfo.title);     // On implémente les données du titre reçues
-                            $idOutput.text(data.items[i].volumeInfo.id);
+                            $idOutput.text(data.items[i].id);
                             $authorOutput.text(data.items[i].volumeInfo.authors);  // On implémente les données de l'auteur reçues
-                            if ($descriptionOutput.text().length<200){
-                            $descriptionOutput.text(data.items[i].volumeInfo.description);
+                            
+                            // On implémente l'image si présent dans la DB sinon image par défaut
+                            let picture;
+                            if(!data.items[i].volumeInfo.imageLinks){
+                                picture = "logo/unavailable.png"; 
+                            } else {
+                                picture = data.items[i].volumeInfo.imageLinks.thumbnail;                  
                             }
-                            // On limite la description à 200 caractères max
-                            console.log($descriptionOutput.text().length);      
-                            if($descriptionOutput.text().length>200){
+                            $bookPicture.attr('src',picture);
+                            //$(".unavailable").attr("src",data.items[i].volumeInfo.imageLinks === undefined ? "logo/unavailable.png" : data.items[i].volumeInfo.imageLinks.thumbnail);
+                            
+                            // On implémente les données de la description en la limitant à 200 caractères max
+                            console.log($descriptionOutput.text().length);   
+                            var desc = data.items[i].volumeInfo.description;   
+                            if(desc){
                                 console.log($descriptionOutput.text());
-                                var desc = data.items[i].volumeInfo.description;
-                                var sub = desc.substring(0,200);
+                                var sub = desc.length>200 ? desc.substring(0,200) +'...' : desc;
                                 console.log(sub);
                                 $descriptionOutput.text(sub);
                                 console.log($descriptionOutput.text().length); 
+                            } else{
+                                $descriptionOutput.text("Pas de description");
                             }
                             $(itemTemplate).clone().appendTo($searchResults);      
-                        })    
                             
-                            $("img").click(function(){
+                            // On switche le bookmark au clic
+                            $bookmarkIcon.click(function(e){
+                                e.preventDefault();
+                                console.log(data.items[i].id)
                                 var src = ($(this).attr("src") === "logo/bookmark-regular.svg") ? "logo/bookmark-solid.svg":"logo/bookmark-regular.svg";
                                 $(this).attr("src",src);
                             })
+                        })    
+                            
+                            
                     },
                     error : function(err){
                         console.log("ça plante",err);
                     }
                 })
+        } else{
+            
         }
     })
     $("#author").keyup(function(){
@@ -112,15 +128,12 @@ $(function(){
         console.log(auteur);
     })
     $search.click(function(){
-        console.log($("#title").val());
-        console.log($("#author").val());
+        console.log($("#titleInput").val());
+        console.log($("#authorInput").val());
     })
 
-    // On switche le bookmark au clic
-    $("img").click(function(){
-        var src = ($(this).attr("src") === "logo/bookmark-regular.svg") ? "logo/bookmark-solid.svg":"logo/bookmark-regular.svg";
-        $(this).attr("src",src);
-    })
+    
+
 
     
 });
