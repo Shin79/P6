7492bad0ae5951316cd76868c6479f602c6d25f7
Shin79 <sg.goto@gmail.com>
2021-01-21 14:57:55 +0100

    Petites modifications dans le code

--- JS/main.js
+++ JS/main.js
@@ -20,29 +20,7 @@ $(function(){
     var $searchResults = $("<section id='searchResults'></section>");
     $("#content").before($searchResults);
     $searchResults.prepend("<h2>Résultats de recherche</h2>"); 
-    $bookModel =  $('<div id="bookModel"></div>');
-    $bookTitle = $('<h4>Titre : </h4>');
-    $titleOutput = $('<span class="bookTitle"></span>');
-    $bookId = $('<h5>Id : </h5>');
-    $idOutput = $('<span class="bookId"></span>');
-    $bookAuthor = $('<h5>Auteur : </h5>');
-    $authorOutput = $('<span class="bookAuthor"></span>')
-    $bookDescription = $('<h5>Description : </h5>');
-    $descriptionOutput = $('<span class="bookDescription"></span>');
-    $bookmarkIcon = $('<img id="bookmark" src="logo/bookmark-regular.svg"/>');
-    $bookPicture = $('<img class="unavailable" src="logo/unavailable.png" alt="couverture du livre" title="Couverture"/>');
-    $('#searchResults').append($bookModel);
-    $($bookTitle).append($titleOutput);
-    $('#bookModel').append($bookTitle);
-    $($bookId).append($idOutput);
-    $('#bookModel').append($bookId);
-    $($bookAuthor).append($authorOutput);
-    $('#bookModel').append($bookAuthor);
-    $($bookDescription).append($descriptionOutput)
-    $('#bookModel').append($bookDescription);
-    $('#bookModel').append($bookPicture);
-    $($bookTitle).prepend($bookmarkIcon);
-    
+
     // On ajoute l'évènement click sur le bouton "Ajouter un livre"
     $addBook.click(function(){
         $form.show();
@@ -54,6 +32,7 @@ $(function(){
         $form.hide();
         $addBook.show();
     })
+
     $(".search").click(function(event){
         event.preventDefault();
         var titre = $("#titleInput").val();
@@ -67,53 +46,13 @@ $(function(){
                     data : titre + auteur,
                     success : function(data){
                         console.log("ça fonctionne",data);
-                        console.log(titre);
-                        var htmlContent = "";
-                        //var itemTemplate = $bookModel;
                         $(data.items).each(function(i){      // for(var i=0;i<data.items.length;i++){
-                            var itemTemplate = $bookModel;
                             console.log(data.items[i]);
                             console.log("Titre : " + data.items[i].volumeInfo.title);
                             console.log("Auteur : " + data.items[i].volumeInfo.authors);
                             console.log(data.items[i].volumeInfo.description);
-                            $titleOutput.text(data.items[i].volumeInfo.title);     // On implémente les données du titre reçues
-                            $idOutput.text(data.items[i].id);
-                            $authorOutput.text(data.items[i].volumeInfo.authors);  // On implémente les données de l'auteur reçues
-                            
-                            // On implémente l'image si présent dans la DB sinon image par défaut
-                            let picture;
-                            if(!data.items[i].volumeInfo.imageLinks){
-                                picture = "logo/unavailable.png"; 
-                            } else {
-                                picture = data.items[i].volumeInfo.imageLinks.thumbnail;                  
-                            }
-                            $bookPicture.attr('src',picture);
-                            //$(".unavailable").attr("src",data.items[i].volumeInfo.imageLinks === undefined ? "logo/unavailable.png" : data.items[i].volumeInfo.imageLinks.thumbnail);
-                            
-                            // On implémente les données de la description en la limitant à 200 caractères max
-                            console.log($descriptionOutput.text().length);   
-                            var desc = data.items[i].volumeInfo.description;   
-                            if(desc){
-                                console.log($descriptionOutput.text());
-                                var sub = desc.length>200 ? desc.substring(0,200) +'...' : desc;
-                                console.log(sub);
-                                $descriptionOutput.text(sub);
-                                console.log($descriptionOutput.text().length); 
-                            } else{
-                                $descriptionOutput.text("Pas de description");
-                            }
-                            $(itemTemplate).clone().appendTo($searchResults);      
-                            
-                            // On switche le bookmark au clic
-                            $bookmarkIcon.click(function(e){
-                                e.preventDefault();
-                                console.log(data.items[i].id)
-                                var src = ($(this).attr("src") === "logo/bookmark-regular.svg") ? "logo/bookmark-solid.svg":"logo/bookmark-regular.svg";
-                                $(this).attr("src",src);
-                            })
+                            $searchResults.append(createBook(data.items[i]));
                         })    
-                            
-                            
                     },
                     error : function(err){
                         console.log("ça plante",err);
@@ -133,8 +72,6 @@ $(function(){
     })
 
     
-
-
     
 });
 
